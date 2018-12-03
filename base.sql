e82ab0e49db44f24b2e21cd1efc413e883c75d4d
Marco Fleres <marco.fleres@iquall.net>
2018-12-03 16:32:22 -0300

    Agregadas vistas 'carta_general' y 'carta_vino'

--- base.sql
+++ base.sql
@@ -16,6 +16,39 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Temporary view structure for view `carta_general`
+--
+
+DROP TABLE IF EXISTS `carta_general`;
+/*!50001 DROP VIEW IF EXISTS `carta_general`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8mb4;
+/*!50001 CREATE VIEW `carta_general` AS SELECT 
+ 1 AS `categoria`,
+ 1 AS `subcategoria`,
+ 1 AS `nombre`,
+ 1 AS `descripcion`,
+ 1 AS `precio`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Temporary view structure for view `carta_vino`
+--
+
+DROP TABLE IF EXISTS `carta_vino`;
+/*!50001 DROP VIEW IF EXISTS `carta_vino`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8mb4;
+/*!50001 CREATE VIEW `carta_vino` AS SELECT 
+ 1 AS `bodega`,
+ 1 AS `nombre`,
+ 1 AS `color`,
+ 1 AS `tamaño`,
+ 1 AS `variedad_uva`,
+ 1 AS `precio`*/;
+SET character_set_client = @saved_cs_client;
+
+--
 -- Table structure for table `categoria`
 --
 
@@ -56,7 +89,7 @@ CREATE TABLE `cobrable` (
   `precio` decimal(20,2) NOT NULL DEFAULT '1.00',
   `tipo` enum('VINO','ITEM_CARTA','MENU_PROMOCIONAL','') NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=63 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -65,7 +98,7 @@ CREATE TABLE `cobrable` (
 
 LOCK TABLES `cobrable` WRITE;
 /*!40000 ALTER TABLE `cobrable` DISABLE KEYS */;
-INSERT INTO `cobrable` VALUES (1,'CREPE DE ALGARROBA','',1.00,'ITEM_CARTA'),(2,'SUPREMA PIRI-PIRI','',1.00,'ITEM_CARTA'),(3,'BIFECITOS DE BONDIOLA','',1.00,'ITEM_CARTA'),(4,'LENGUADO NORMANDA','',1.00,'ITEM_CARTA'),(5,'BIFE DE CHORIZO RELLENO','',1.00,'ITEM_CARTA'),(6,'LOMO BLACK','',1.00,'ITEM_CARTA'),(7,'OJO DE BIFE MALBEC','',1.00,'ITEM_CARTA'),(8,'ENTRAÑAS GRILLÉ','',1.00,'ITEM_CARTA'),(9,'BAILEYS FROZEN','',1.00,'ITEM_CARTA'),(10,'COPA FINSUR','',1.00,'ITEM_CARTA'),(11,'CUERDAS DE GUITARRA AL WOK','',1.00,'ITEM_CARTA'),(12,'PECHUGA AL VERDEO','',1.00,'ITEM_CARTA'),(13,'MERLUZA A LA ESPAÑOLA','',1.00,'ITEM_CARTA'),(14,'LIGHT (FRÍO)','',1.00,'ITEM_CARTA'),(15,'DE ATÚN (FRÍO)','',1.00,'ITEM_CARTA'),(16,'VERDE (CALIENTE)','',1.00,'ITEM_CARTA'),(17,'ORIENTAL (CALIENTE)','',1.00,'ITEM_CARTA'),(18,'DE ATÚN','',1.00,'ITEM_CARTA'),(19,'DE SALMÓN','',1.00,'ITEM_CARTA'),(20,'Mejillones a la Provenzal o al Ajillo','',1.00,'ITEM_CARTA'),(21,'Gambas al Ajillo','',1.00,'ITEM_CARTA'),(22,'Rabas','',1.00,'ITEM_CARTA'),(23,'Mayonesa de Ave','',1.00,'ITEM_CARTA'),(24,'Mayonesa de Atún','',1.00,'ITEM_CARTA'),(25,'Tomate Relleno','',1.00,'ITEM_CARTA'),(26,'Empanada de Carne, Pollo o Jamón y Queso Empanada de Verdura o Humita','',1.00,'ITEM_CARTA'),(27,'Hasta tres ingredientes','',1.00,'ITEM_CARTA'),(28,'Hasta cinco ingredientes','',1.00,'ITEM_CARTA'),(29,'Completa','',1.00,'ITEM_CARTA'),(30,'CÉSAR','',1.00,'ITEM_CARTA'),(31,'DEL CHEF','',1.00,'ITEM_CARTA'),(32,'CENTENARIO','',1.00,'ITEM_CARTA'),(33,'LULI','',1.00,'ITEM_CARTA'),(34,'POPEYE','',1.00,'ITEM_CARTA'),(35,'EMILIANO','',1.00,'ITEM_CARTA'),(36,'RÚCULA Y PARMESANO','',1.00,'ITEM_CARTA'),(37,'Jamón Crudo','',1.00,'ITEM_CARTA'),(38,'Jamón Cocido','',1.00,'ITEM_CARTA'),(39,'Salame','',1.00,'ITEM_CARTA'),(40,'Leverwust','',1.00,'ITEM_CARTA'),(41,'Hamburguesa','',1.00,'ITEM_CARTA'),(42,'Pollo','',1.00,'ITEM_CARTA'),(43,'Lomo','',1.00,'ITEM_CARTA'),(44,'Milanesa','',1.00,'ITEM_CARTA'),(45,'Queso','',1.00,'ITEM_CARTA'),(46,'Tomate y Lechuga','',1.00,'ITEM_CARTA'),(47,'Huevo Duro','',1.00,'ITEM_CARTA'),(48,'Ajíes','',1.00,'ITEM_CARTA'),(49,'Pickles','',1.00,'ITEM_CARTA'),(50,'Ñoquis','',1.00,'ITEM_CARTA'),(51,'Tallarines','',1.00,'ITEM_CARTA'),(52,'Spaguettis','',1.00,'ITEM_CARTA'),(53,'Ñoquis Rellenos (blancos o verdes)','',1.00,'ITEM_CARTA'),(54,'Ravioles','',1.00,'ITEM_CARTA'),(55,'Sorrentinos','',1.00,'ITEM_CARTA'),(56,'Canelones','',1.00,'ITEM_CARTA'),(57,'Lasagna','',1.00,'ITEM_CARTA'),(58,'Crema de Jamón','',1.00,'ITEM_CARTA'),(59,'Parisién','',1.00,'ITEM_CARTA'),(60,'Cuatro Quesos','',1.00,'ITEM_CARTA'),(61,'Pesto','',1.00,'ITEM_CARTA'),(62,'Carbonara','',1.00,'ITEM_CARTA');
+INSERT INTO `cobrable` VALUES (1,'CREPE DE ALGARROBA','',1.00,'ITEM_CARTA'),(2,'SUPREMA PIRI-PIRI','',1.00,'ITEM_CARTA'),(3,'BIFECITOS DE BONDIOLA','',1.00,'ITEM_CARTA'),(4,'LENGUADO NORMANDA','',1.00,'ITEM_CARTA'),(5,'BIFE DE CHORIZO RELLENO','',1.00,'ITEM_CARTA'),(6,'LOMO BLACK','',1.00,'ITEM_CARTA'),(7,'OJO DE BIFE MALBEC','',1.00,'ITEM_CARTA'),(8,'ENTRAÑAS GRILLÉ','',1.00,'ITEM_CARTA'),(9,'BAILEYS FROZEN','',1.00,'ITEM_CARTA'),(10,'COPA FINSUR','',1.00,'ITEM_CARTA'),(11,'CUERDAS DE GUITARRA AL WOK','',1.00,'ITEM_CARTA'),(12,'PECHUGA AL VERDEO','',1.00,'ITEM_CARTA'),(13,'MERLUZA A LA ESPAÑOLA','',1.00,'ITEM_CARTA'),(14,'LIGHT (FRÍO)','',1.00,'ITEM_CARTA'),(15,'DE ATÚN (FRÍO)','',1.00,'ITEM_CARTA'),(16,'VERDE (CALIENTE)','',1.00,'ITEM_CARTA'),(17,'ORIENTAL (CALIENTE)','',1.00,'ITEM_CARTA'),(18,'DE ATÚN','',1.00,'ITEM_CARTA'),(19,'DE SALMÓN','',1.00,'ITEM_CARTA'),(20,'Mejillones a la Provenzal o al Ajillo','',1.00,'ITEM_CARTA'),(21,'Gambas al Ajillo','',1.00,'ITEM_CARTA'),(22,'Rabas','',1.00,'ITEM_CARTA'),(23,'Mayonesa de Ave','',1.00,'ITEM_CARTA'),(24,'Mayonesa de Atún','',1.00,'ITEM_CARTA'),(25,'Tomate Relleno','',1.00,'ITEM_CARTA'),(26,'Empanada de Carne, Pollo o Jamón y Queso Empanada de Verdura o Humita','',1.00,'ITEM_CARTA'),(27,'Hasta tres ingredientes','',1.00,'ITEM_CARTA'),(28,'Hasta cinco ingredientes','',1.00,'ITEM_CARTA'),(29,'Completa','',1.00,'ITEM_CARTA'),(30,'CÉSAR','',1.00,'ITEM_CARTA'),(31,'DEL CHEF','',1.00,'ITEM_CARTA'),(32,'CENTENARIO','',1.00,'ITEM_CARTA'),(33,'LULI','',1.00,'ITEM_CARTA'),(34,'POPEYE','',1.00,'ITEM_CARTA'),(35,'EMILIANO','',1.00,'ITEM_CARTA'),(36,'RÚCULA Y PARMESANO','',1.00,'ITEM_CARTA'),(37,'Jamón Crudo','',1.00,'ITEM_CARTA'),(38,'Jamón Cocido','',1.00,'ITEM_CARTA'),(39,'Salame','',1.00,'ITEM_CARTA'),(40,'Leverwust','',1.00,'ITEM_CARTA'),(41,'Hamburguesa','',1.00,'ITEM_CARTA'),(42,'Pollo','',1.00,'ITEM_CARTA'),(43,'Lomo','',1.00,'ITEM_CARTA'),(44,'Milanesa','',1.00,'ITEM_CARTA'),(45,'Queso','',1.00,'ITEM_CARTA'),(46,'Tomate y Lechuga','',1.00,'ITEM_CARTA'),(47,'Huevo Duro','',1.00,'ITEM_CARTA'),(48,'Ajíes','',1.00,'ITEM_CARTA'),(49,'Pickles','',1.00,'ITEM_CARTA'),(50,'Ñoquis','',1.00,'ITEM_CARTA'),(51,'Tallarines','',1.00,'ITEM_CARTA'),(52,'Spaguettis','',1.00,'ITEM_CARTA'),(53,'Ñoquis Rellenos (blancos o verdes)','',1.00,'ITEM_CARTA'),(54,'Ravioles','',1.00,'ITEM_CARTA'),(55,'Sorrentinos','',1.00,'ITEM_CARTA'),(56,'Canelones','',1.00,'ITEM_CARTA'),(57,'Lasagna','',1.00,'ITEM_CARTA'),(58,'Crema de Jamón','',1.00,'ITEM_CARTA'),(59,'Parisién','',1.00,'ITEM_CARTA'),(60,'Cuatro Quesos','',1.00,'ITEM_CARTA'),(61,'Pesto','',1.00,'ITEM_CARTA'),(62,'Carbonara','',1.00,'ITEM_CARTA'),(63,'Michele Torino','Botella 700ml',1.00,'VINO'),(64,'Santa Ana','Botella 700ml',1.00,'VINO');
 /*!40000 ALTER TABLE `cobrable` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -278,9 +311,7 @@ CREATE TABLE `paso` (
   `nombre_item` varchar(45) NOT NULL,
   `tamaño_item` varchar(45) NOT NULL,
   PRIMARY KEY (`nombre_menu`,`tamaño_menu`,`nombre_item`,`tamaño_item`),
-  KEY `fk_paso_item` (`nombre_item`,`tamaño_item`),
-  CONSTRAINT `fk_paso_item` FOREIGN KEY (`nombre_item`, `tamaño_item`) REFERENCES `item_carta` (`nombre`, `tamaño`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_paso_menu` FOREIGN KEY (`nombre_menu`, `tamaño_menu`) REFERENCES `menu_promocional` (`nombre`, `tamaño`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `fk_paso_item` (`nombre_item`,`tamaño_item`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -312,9 +343,7 @@ CREATE TABLE `solicitud` (
   `observaciones` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_solicitud_cobrable` (`nombre_cobrable`,`tamaño_cobrable`),
-  KEY `fk_solicitud_comanda` (`fecha_comanda`,`mesa_comanda`),
-  CONSTRAINT `fk_solicitud_cobrable` FOREIGN KEY (`nombre_cobrable`, `tamaño_cobrable`) REFERENCES `cobrable` (`nombre`, `tamaño`),
-  CONSTRAINT `fk_solicitud_comanda` FOREIGN KEY (`fecha_comanda`, `mesa_comanda`) REFERENCES `comanda` (`fecha`, `mesa`)
+  KEY `fk_solicitud_comanda` (`fecha_comanda`,`mesa_comanda`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -350,8 +379,45 @@ CREATE TABLE `vino` (
 
 LOCK TABLES `vino` WRITE;
 /*!40000 ALTER TABLE `vino` DISABLE KEYS */;
+INSERT INTO `vino` VALUES (63,'Tinto','La Rosa',NULL),(64,'Tinto','',NULL);
 /*!40000 ALTER TABLE `vino` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Final view structure for view `carta_general`
+--
+
+/*!50001 DROP VIEW IF EXISTS `carta_general`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_unicode_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `carta_general` AS select coalesce(`categoria_padre`.`nombre`,`categoria`.`nombre`) AS `categoria`,(case when isnull(`categoria`.`padre`) then NULL else `categoria`.`nombre` end) AS `subcategoria`,`cobrable`.`nombre` AS `nombre`,`item_carta`.`descripcion` AS `descripcion`,`cobrable`.`precio` AS `precio` from (((`item_carta` join `cobrable` on((`item_carta`.`cobrable` = `cobrable`.`id`))) join `categoria` on((`item_carta`.`categoria` = `categoria`.`id`))) left join `categoria` `categoria_padre` on((`categoria`.`padre` = `categoria_padre`.`id`))) order by coalesce(`categoria_padre`.`nombre`,`categoria`.`nombre`),(case when isnull(`categoria`.`padre`) then NULL else `categoria`.`nombre` end),`cobrable`.`nombre` */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `carta_vino`
+--
+
+/*!50001 DROP VIEW IF EXISTS `carta_vino`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_unicode_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `carta_vino` AS select `vino`.`bodega` AS `bodega`,`cobrable`.`nombre` AS `nombre`,`vino`.`color` AS `color`,`cobrable`.`tamaño` AS `tamaño`,`vino`.`variedad_uva` AS `variedad_uva`,`cobrable`.`precio` AS `precio` from (`vino` join `cobrable` on((`vino`.`cobrable` = `cobrable`.`id`))) order by `vino`.`bodega`,`cobrable`.`nombre` */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -362,4 +428,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-12-01 15:47:26
+-- Dump completed on 2018-12-03 16:30:06
